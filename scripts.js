fec1865a93781da54d1c9338846017ab448a09ce
amir.rismaani <amir.rismaani@gmail.com>
2022-02-20 19:41:47 +0330

    add scss style

--- scripts.js
+++ scripts.js
@@ -20,32 +20,31 @@ export default class InvoiceOrder {
         const shop = this.data.shop;
         const order = this.data.order;
         this.html += `
-            <table id="header">
+            <table id="header" width="100%">
                 <colgroup>
                     <col span="1" style="width: 50%;">
                     <col span="1" style="width: 50%;">
                 </colgroup>
                 <tbody>
                     <tr>
-                        <td>${headerConstants.shop} ${shop.name}</td>
-                        <td>${headerConstants.cashierName} ${shop.cashier}</td>
+                        <td class="title title--lg bold">${headerConstants.shop} ${shop.name}</td>
+                        <td>${headerConstants.cashierName} <span class="bold">${shop.cashier}</span></td>
                     </tr>
                     <tr>
-                        <td>${headerConstants.saleOrder}</td>
+                        <td class="title title--md bold">${headerConstants.saleOrder}</td>
                         <td>
-                            ${shop.address}
-                            ${shop.phone}
+                            ${shop.address} - ${shop.phone}
                         </td>
                     </tr>                    
                     <tr>
-                        <td colspan="2">${headerConstants.customer.name} ${customer.name}</td>
+                        <td colspan="2">${headerConstants.customer.name} <span class="bold">${customer.name}</span></td>
                     </tr>
                     <tr>
-                        <td colspan="2">${headerConstants.customer.address} ${customer.address}</td>
+                        <td colspan="2">${headerConstants.customer.address}  ${customer.address}</td>
                     </tr>
                     <tr>
-                        <td>${headerConstants.order.code} ${order.code}</td>
-                        <td>${headerConstants.order.dateTime} ${order.time} - ${order.date}</td>
+                        <td class="border-top border-bottom">${headerConstants.order.code} <span class="bold">${order.code}</span></td>
+                        <td class="border-top border-bottom">${headerConstants.order.dateTime} <span class="bold">${order.time} - ${order.date}</span></td>
                     </tr>
                 </tbody>
             </table>
@@ -61,7 +60,7 @@ export default class InvoiceOrder {
 
 
         this.html += `
-            <table id="content">
+            <table id="content" width="100%">
                 <colgroup>
                     <col span="1" style="width: 5%;">
                     <col span="1" style="width: 30%;">
@@ -74,28 +73,28 @@ export default class InvoiceOrder {
 
                 <thead>
                     <tr>
-                        <th>${orderLinesConstants.row}</th>
-                        <th>${orderLinesConstants.productName}</th>
-                        <th>${orderLinesConstants.unitPrice}</th>
-                        <th>${orderLinesConstants.quantity}</th>
-                        <th>${orderLinesConstants.tax}</th>
-                        <th>${orderLinesConstants.discount}</th>
-                        <th>${orderLinesConstants.total}</th>
+                        <th class="border-right--none border-bottom--double">${orderLinesConstants.row}</th>
+                        <th class="border-bottom--double">${orderLinesConstants.productName}</th>
+                        <th class="border-bottom--double">${orderLinesConstants.unitPrice}</th>
+                        <th class="border-bottom--double">${orderLinesConstants.quantity}</th>
+                        <th class="border-bottom--double">${orderLinesConstants.tax}</th>
+                        <th class="border-bottom--double">${orderLinesConstants.discount}</th>
+                        <th class="border-bottom--double">${orderLinesConstants.total}</th>
                     </tr>
                 </thead>
                 <tbody>
                 ${orderLines.map((orderLine, index) => {
             return `
-                                <tr>
-                                    <td>${index++}</td>
-                                    <td>${orderLine.name}</td>
-                                    <td>${orderLine.price}</td>
-                                    <td>${orderLine.quantity}\n${orderLine.unit}</td>
-                                    <td>${orderLine.tax}</td>
-                                    <td>${orderLine.discount}</td>
-                                    <td>${orderLine.total}</td>
-                                </tr>
-                            `
+                    <tr>
+                        <td class="border-right--none">${index++}</td>
+                        <td>${orderLine.name}</td>
+                        <td>${orderLine.price}</td>
+                        <td>${orderLine.quantity}\n${orderLine.unit}</td>
+                        <td>${orderLine.tax}</td>
+                        <td>${orderLine.discount}</td>
+                        <td>${orderLine.total}</td>
+                    </tr>
+                `
         }).join("")}
                 </tbody>
             </table>
@@ -108,44 +107,44 @@ export default class InvoiceOrder {
         const order = this.data.order;
 
         this.html += `
-            <table id = "footer">
+            <table id = "footer" width="100%">
                 <colgroup>
-                    <col span="1" style="width: 35%;">
-                    <col span="1" style="width: 65%;">
+                    <col span="1" style="width: 35.7%;">
+                    <col span="1" style="width: 64.3%;">
                 </colgroup>
 
                 <tbody>
                     <tr>
-                        <td>${orderConstants.totalAmount}</td>
-                        <td>${order.totalAmount}</td>
+                        <td class="border-right--none border-top--double">${orderConstants.totalAmount}</td>
+                        <td class="border-top--double">${order.totalAmount}</td>
                     </tr>
                     <tr>
-                        <td>${orderConstants.totalTax}</td>
+                        <td class="border-right--none">${orderConstants.totalTax}</td>
                         <td>${order.totalTax}</td>
                     </tr>
                     <tr>
-                        <td>${orderConstants.totalDiscount}</td>
+                        <td class="border-right--none">${orderConstants.totalDiscount}</td>
                         <td>${order.totalDiscount}</td>
                     </tr>
                     <tr>
-                        <td>${orderConstants.gift}</td>
+                        <td class="border-right--none">${orderConstants.gift}</td>
                         <td>${order.gift}</td>
                     </tr>
                     <tr>
-                        <td>${orderConstants.finalAmount}</td>
+                        <td class="border-right--none">${orderConstants.finalAmount}</td>
                         <td>${order.finalAmount}</td>
                     </tr>
                     <tr>
-                        <td>${orderConstants.paidAmount}</td>
+                        <td class="border-right--none">${orderConstants.paidAmount}</td>
                         <td>${order.paidAmount}</td>
                     </tr>
                     <tr>
-                        <td>${orderConstants.reminderAmount}</td>
+                        <td class="border-right--none">${orderConstants.reminderAmount}</td>
                         <td>${order.reminderAmount}</td>
                     </tr>
                     <tr>
-                        <td>${footerConstants.qrDesc}</td>
-                        <td>${footerConstants.barcodeDesc}</td>
+                        <td class="border-bottom--none border-right--none border-top--double">${footerConstants.qrDesc}</td>
+                        <td class="border-bottom--none border-top--double">${footerConstants.barcodeDesc}</td>
                     </tr>
                 </tbody>
             </table>
